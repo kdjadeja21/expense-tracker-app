23439552e0f7c4c0284c19d18770b95ae543a1bc
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2024-09-09 13:49:01 +0530

    Added navigation and header

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import authros from "./authors";
+import books from "./books";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.route("/authors", authros);
+app.route("/books", books);
+
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({ error: "Unauthorized" });
+  }
+
+  return c.json({
+    message: "Hello Next.js!",
+    userId: auth.userId,
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { HeaderLogo } from "./header-logo";
+import { Navigation } from "@components/Navigation";
+import { WelcomeMsg } from "@components/welcome-msg";
+
+export const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-6">
+            <HeaderLogo />
+            <Navigation />
+          </div>
+          <ClerkLoaded>
+            <UserButton afterSwitchSessionUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-400" />
+          </ClerkLoading>
+        </div>
+        <WelcomeMsg />
+      </div>
+    </header>
+  );
+};
